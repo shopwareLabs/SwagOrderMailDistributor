6990cf9654292f8e4aa198c9532d7429863a163a
Oliver Skroblin <o.skroblin@shopware.com>
2020-03-26 09:16:02 +0100

    Added admin module for distributions

---
+++ src/Resources/app/administration/src/module/swag-order-mail-distributor/index.js
@@ -0,0 +1,57 @@
+import './page/swag-order-mail-distributor-list';
+import './page/swag-order-mail-distributor-detail';
+import './core-fix/sw-fix-entity-single-select';
+
+const { Module } = Shopware;
+
+Module.register('swag-order-mail-distributor', {
+    type: 'core',
+    name: 'distribution',
+    title: 'swag-order-mail-distributor.general.mainMenuItemGeneral',
+    description: 'Manages the distribution of the application',
+    version: '1.0.0',
+    targetVersion: '1.0.0',
+    color: '#57D9A3',
+    icon: 'default-symbol-products',
+    favicon: 'icon-module-products.png',
+    entity: 'order_mail_distribution',
+
+    routes: {
+        index: {
+            components: {
+                default: 'swag-order-mail-distributor-list'
+            },
+            path: 'index'
+        },
+        create: {
+            component: 'swag-order-mail-distributor-detail',
+            path: 'create',
+            meta: {
+                parentPath: 'swag.order.mail.distributor.index'
+            }
+        },
+        detail: {
+            component: 'swag-order-mail-distributor-detail',
+            path: 'detail/:id',
+            meta: {
+                parentPath: 'swag.order.mail.distributor.index'
+            },
+            props: {
+                default(route) {
+                    return {
+                        distributionId: route.params.id
+                    };
+                }
+            }
+        }
+    },
+
+    navigation: [{
+        path: 'swag.order.mail.distributor.index',
+        label: 'swag-order-mail-distributor.general.mainMenuItemList',
+        id: 'swag-order-mail-distributor',
+        parent: 'sw-order',
+        color: '#57D9A3',
+        position: 50
+    }]
+});
