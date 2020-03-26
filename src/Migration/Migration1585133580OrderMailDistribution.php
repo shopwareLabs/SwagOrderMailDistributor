4f4420a804cb3bd879c695ccd8ad651e2ddb703d
s.sluiter <s.sluiter@shopware.com>
2020-03-26 08:35:17 +0100

    NTR - fix foreign key

--- src/Migration/Migration1585133580OrderMailDistribution.php
+++ src/Migration/Migration1585133580OrderMailDistribution.php
@@ -25,7 +25,7 @@ class Migration1585133580OrderMailDistribution extends MigrationStep
               `updated_at`  DATETIME(3) NULL,
               PRIMARY KEY (`id`),
               CONSTRAINT `fk.order_mail_distribution.mail_template_id`
-                FOREIGN KEY (`id`) REFERENCES `mail_template` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                FOREIGN KEY (`mail_template_id`) REFERENCES `mail_template` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         ');
     }
